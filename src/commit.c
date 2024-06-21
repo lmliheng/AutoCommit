8f6490e043cf457a156c75a73a4aaf967cb961d7
liheng <liheng2137@126.com>
2024-06-21 21:24:25 +0800

    自动签到提交代码

--- src/commit.c
+++ src/commit.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
-#include<time.h>
+#include <time.h>
 #include<string.h>
 #include<stdlib.h>
-#include<unistd.h> 
+#include<unistd.h>
+#include <sys/wait.h>
+
+void execute_command(const char *command) {
+    pid_t pid = fork();
+
+    if (pid == 0) { // 子进程
+        execl("/bin/sh", "sh", "-c", command, NULL);
+        perror("execl");
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) { // fork 失败
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main() {
     // 获取当前时间
@@ -17,45 +31,76 @@ int main() {
     // 创建或打开 log/commit_log 文件
     FILE *file = fopen("/root/c/AutoCommit_push/log/commit.log", "a");
     if (file == NULL) {
-        printf("无法打开日志文件！\n");
+        printf("无法打开日志文件！");
         return 1;
     }
 
     // 向文件中写入当前时间
-    fprintf(file, "提交时间: %s\n", buffer);  // 提交可能失败
+    fprintf(file, "提交时间: %s", buffer);
 
     // 关闭文件
     fclose(file);
 
-    system("rm -rf /root/c/AutoCommit_push/AutoCommit");
+    pid_t child_pid;
+    int status;
 
-    printf("删除拉取目录一次结束\n");
+    child_pid = fork();
+    if (child_pid == 0) {
+        execute_command("rm -rf /root/c/AutoCommit_push/AutoCommit");
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(child_pid, &status, 0);
+        printf("删除拉取目录一次结束");
+    }
 
-    printf("日志已经写入 /root/c/AutoCommit_push/log/commit.log\n");
+    printf("日志已经写入 /root/c/AutoCommit_push/log/commit.log");
 
+    printf("删除拉取目录结束");
 
-
-    printf("删除拉取目录结束\n");
-
-    system("bash /root/c/AutoCommit_push/clone.sh");
-
-    printf("成功拉取新目录\n");
+    child_pid = fork();
+    if (child_pid == 0) {
+        execute_command("bash /root/c/AutoCommit_push/clone.sh");
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(child_pid, &status, 0);
+        printf("成功拉取新目录");
+    }
 
     sleep(5);
 
-    system("yes | cp -rf /root/c/AutoCommit_push/AutoCommit/.git .");
+    child_pid = fork();
+    if (child_pid == 0) {
+        execute_command("yes | cp -rf /root/c/AutoCommit_push/AutoCommit/.git .");
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(child_pid, &status, 0);
+        printf("成功复制git历史提交记录");
+    }
 
-    printf("成功复制git历史提交记录\n");
-    
-    system("git remote set-url origin git@github.com:lmliheng/AutoCommit.git");
+    child_pid = fork();
+    if (child_pid == 0) {
+        execute_command("git remote set-url origin git@github.com:lmliheng/AutoCommit.git");
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(child_pid, &status, 0);
+    }
 
-    system("bash /root/c/AutoCommit_push/push.sh");
+    child_pid = fork();
+    if (child_pid == 0) {
+        execute_command("bash /root/c/AutoCommit_push/push.sh");
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(child_pid, &status, 0);
+    }
 
-
-    system("rm -rf /root/c/AutoCommit_push/AutoCommit");
-
-    printf("删除拉取目录二次结束\n");
-
+    child_pid = fork();
+    if (child_pid == 0) {
+        execute_command("rm -rf /root/c/AutoCommit_push/AutoCommit");
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(child_pid, &status, 0);
+        printf("删除拉取目录二次结束");
+    }
 
     return 0;
 }
